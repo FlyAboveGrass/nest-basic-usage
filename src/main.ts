ee3594f0b8aa757f65de261f97d101313b6495ae
杨家健 <287023075@qq.com>
2023-12-02 23:08:03 +0800

    全局拦截器

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RoleGuard } from './guard/role/role.guard';
 import { LoggingInterceptor } from './interceptors/logging/logging.interceptor';
+import { TransformInterceptor } from './interceptors/transform/transform.interceptor';
 
 declare const module: any;
 
@@ -9,6 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalGuards(new RoleGuard());
   app.useGlobalInterceptors(new LoggingInterceptor()); // 全局共用该示例
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   await app.listen(3000);
 
