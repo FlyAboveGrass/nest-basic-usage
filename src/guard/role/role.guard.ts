3ef87eae2439ed9a20f5a2109bf335fd799b3fd2
杨家健 <287023075@qq.com>
2023-12-02 22:10:35 +0800

    全局路由守卫

---
+++ src/guard/role/role.guard.ts
@@ -0,0 +1,12 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    // console.log('🚀-  ->  全局守卫 触发！');
+    return true;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RoleGuard } from './guard/role/role.guard';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalGuards(new RoleGuard());
   await app.listen(3000);
 
   if (module.hot) {
